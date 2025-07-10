f6e1da7ae1a2041d16536b751206e96bb28d2d44
dripdontkillmyvibe <132927699+dripdontkillmyvibe@users.noreply.github.com>
2025-07-10 17:20:29 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
-    /** @type {import('tailwindcss').Config} */
-    module.exports = {
-      content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-      ],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-blue': '#17479E',
+        'brand-green': '#00B14B',
+        'brand-yellow': '#FFD100',
+        'brand-red': '#E40646',
+      }
+    },
+    fontFamily: {
+      sans: ['Proxima Nova', 'sans-serif'],
     }
-    
+  },
+  plugins: [],
+}
